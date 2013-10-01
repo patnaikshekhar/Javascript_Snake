8b08c547c26300537261ae144273a625ee0629d6
Shekhar Patnaik <shpatnaik@deloitte.com>
2013-10-01 14:46:42 +0530

    Added Multiplayer support, Modularized Snake and Food classes

--- snake.js
+++ snake.js
@@ -3,11 +3,11 @@
 var CANVAS_HEIGHT = 500;
 var CANVAS_WIDTH = 1000;
 
-var BOX_HEIGHT = 10;
-var BOX_WIDTH = 10;
+var BOX_HEIGHT = 20;
+var BOX_WIDTH = 20;
 
-var BOARD_ROWS = CANVAS_HEIGHT / BOX_HEIGHT;
-var BOARD_COLUMNS = CANVAS_WIDTH / BOX_WIDTH;
+var BOARD_ROWS = Math.round(CANVAS_HEIGHT / BOX_HEIGHT);
+var BOARD_COLUMNS = Math.round(CANVAS_WIDTH / BOX_WIDTH);
 
 var EMPTY_BOX = 0;
 var SNAKE_BOX = 1;
@@ -15,8 +15,6 @@ var OBSTICLE_BOX = 2;
 var FOOD_BOX = 3;
 
 var SNAKE_START_LENGTH = 5;
-var SNAKE_START_ROW = BOARD_ROWS / 2;
-var SNAKE_START_COLUMN = BOARD_COLUMNS / 2;
 var SNAKE_START_SPEED = 4;
 var SNAKE_SIZE_INCREASE_FACTOR = 5;
 
@@ -27,26 +25,165 @@ var DIRECTION_DOWN = 1;
 var DIRECTION_LEFT = 2;
 var DIRECTION_RIGHT = 3;
 
-var SCORE_LOCATION_X = CANVAS_WIDTH - 100;
-var SCORE_LOCATION_Y = 20;
+var SCORE_LOCATION_1_X = CANVAS_WIDTH - 100;
+var SCORE_LOCATION_1_Y = 20;
+
+var SCORE_LOCATION_2_X = CANVAS_WIDTH - 100;
+var SCORE_LOCATION_2_Y = 40;
+
+var RANDOM_COLORS = ["#FF0000","#00FF00", "#0000FF", "#FFFF00", "#00FFFF", "#FF00FF", "CC0099", "9933CC", "9933FF", "FF6666", "FFFF33", "996666", "6699FF"];
 
 // End Constants
 
 var ctx = null;  // Holds the Drawing Context
-var board = []; // Holds the Drawing Board
 
-var direction = DIRECTION_LEFT; // Current Direction of the Snake
-var speedCalc = 0; // This variable is used to Skip Frames
-var speed = SNAKE_START_SPEED; // This variable holds the current speed
+// Holds the global snake class
+var player1Snake = null;
+var player2Snake = null;
+var food = null;
 
-var foodPosition = null;
-var score = 0;
+// Singleton - It holds all snakes
+SnakesOnBoard = {
+	snakes: [],
+	newSnakeId: 0,
 
-var snake = {
-	queue: [],
-	size: SNAKE_START_LENGTH,
+	add: function() {
 
-	add: function(position) {
+		var snake = new Snake();
+		snake.id = this.newSnakeId;
+		this.newSnakeId++;
+
+		this.snakes.push(snake);
+
+		return snake.id;
+	},
+
+	getIndex: function(id) {
+		var index = -1;
+
+		for (var i = 0; i < this.snakes.length; i++) {
+			if (this.snakes[i].id == id) {
+				index = i;
+				break;
+			}
+		}
+
+		return index;
+	},
+
+	getSnake: function(id) {
+		var index = this.getIndex(id);
+
+		if (index >= 0) {
+			return this.snakes[index];
+		}		
+
+		return null;
+	},
+
+	remove: function(id) {
+
+		var index = this.getIndex(id);
+
+		if (index >= 0) {
+			for (var i = index; i < this.snakes.length - 1; i++) {
+				this.snakes[i] = this.snakes[i + 1];
+			}	
+		}
+				
+		this.snakes.pop();
+	},
+
+	draw: function(context) {
+
+		for (var i = 0; i < this.snakes.length; i++) {
+			this.snakes[i].draw(context);
+		}
+	},
+
+	update: function() {
+
+		for (var i = 0; i < this.snakes.length; i++) {
+			this.snakes[i].update();
+		}
+	},
+
+	contains: function(location) {
+
+		for (var i = 0; i < this.snakes.length; i++) {
+			if(this.snakes[i].contains(location))
+				return true;
+		}
+
+		return false;	
+	}
+};
+
+//_____________________________________________________________________________________
+// Start Class Food
+
+function Food() {
+	this.location = [];
+	this.color = getRandomColor();
+
+	this.createRandomFood();
+}
+
+// This function returns a random position for Food
+Food.prototype.createRandomFood = function() {
+		var row = Math.floor((Math.random() * BOARD_ROWS - 1) + 1);
+		var column = Math.floor((Math.random() * BOARD_COLUMNS - 1) + 1);
+
+		this.location = [row, column];
+	};
+
+Food.prototype.draw = 
+	function(context) {
+		var coord = getCoordsOfBox(this.location[0], this.location[1]);
+		context.fillStyle = this.color;
+		context.fillRect(coord.x, coord.y, BOX_WIDTH, BOX_HEIGHT);
+	};
+
+// End Class Food
+
+// Start Class Snake
+
+function Snake() {
+
+	this.initialize();
+}
+
+Snake.prototype.initialize = 
+	function() {
+		
+		this.id = -1;
+
+		this.score = 0; // Holds the score of the current player
+		this.direction = DIRECTION_LEFT; // Current Direction of the Snake
+		this.speedCalc = 0; // This variable is used to Skip Frames
+		this.speed = SNAKE_START_SPEED; // This variable holds the current speed
+		
+		this.queue = [];
+		this.size = SNAKE_START_LENGTH;
+
+		this.color = getRandomColor();
+
+		this.getRandomLocation();
+	};
+
+Snake.prototype.getRandomLocation = 
+	function() {
+		var startRow = Math.floor((Math.random() * BOARD_ROWS - SNAKE_START_LENGTH) + 0);
+		var startColumn = Math.floor((Math.random() * BOARD_COLUMNS - SNAKE_START_LENGTH) + 0);
+
+		for (var columnShift = SNAKE_START_LENGTH - 1; columnShift >= 0; columnShift--) {
+			this.add([startRow, startColumn + columnShift]);
+		}
+	};
+
+Snake.prototype.add = 
+	
+	function(position) {
 
 		this.queue.push(position);
 
@@ -55,29 +192,35 @@ var snake = {
 		} else {
 			return null;
 		}
-	},
+	};
 
-	remove: function() {
+Snake.prototype.remove = 
+	function() {
 		return this.queue.shift();
-	},
+	};
 
-	length: function() {
+Snake.prototype.length = 
+	function() {
 		return this.queue.length;
-	},
+	};
 
-	head: function() {
+Snake.prototype.head = 
+	function() {
 		return this.queue[this.length() - 1];
-	},
+	};
 
-	headX: function() {
+Snake.prototype.headX = 
+	function() {
 		return this.head()[1];
-	},
+	};
 
-	headY: function() {
+Snake.prototype.headY = 
+	function() {
 		return this.head()[0];
-	},
+	};
 
-	contains: function(lst) {
+Snake.prototype.contains = 
+	function(lst) {
 		var row = lst[0];
 		var column = lst[1];
 		
@@ -87,13 +230,70 @@ var snake = {
 		} 
 
 		return false;
-	}
-};
+	};
+
+Snake.prototype.draw =
+	function(context) {
+		for (var i = 0; i < this.queue.length; i++) {
+			var coord = getCoordsOfBox(this.queue[i][0], this.queue[i][1]);
+			context.fillStyle = this.color;
+			context.fillRect(coord.x, coord.y, BOX_WIDTH, BOX_HEIGHT);
+		}		
+	};
+
+Snake.prototype.update = 
+// This function updates the position of the Snake
+	function() {
+
+		if (this.speedCalc >= this.speed) {
+			var snakeCurrentPositionX = this.headX();
+			var snakeCurrentPositionY = this.headY();
+
+			if (this.direction == DIRECTION_LEFT) {
+				snakeCurrentPositionX -= 1;		
+			} else if (this.direction == DIRECTION_RIGHT) {
+				snakeCurrentPositionX += 1;
+			} else if (this.direction == DIRECTION_UP) {
+				snakeCurrentPositionY -= 1;
+			} else {
+				snakeCurrentPositionY += 1;
+			}
+
+			// Wrap Around
+			if (snakeCurrentPositionX < 0) {
+				snakeCurrentPositionX = BOARD_COLUMNS - 1;
+			} else if (snakeCurrentPositionX >= BOARD_COLUMNS) {
+				snakeCurrentPositionX = 0;
+			} else if (snakeCurrentPositionY < 0) {
+				snakeCurrentPositionY = BOARD_ROWS - 1;
+			} else if (snakeCurrentPositionY >= BOARD_ROWS) {
+				snakeCurrentPositionY = 0;
+			} 
+
+			collisionCheck(this, snakeCurrentPositionX, snakeCurrentPositionY);
+
+			this.add([snakeCurrentPositionY, snakeCurrentPositionX]);
+			
+			this.speedCalc = 0;
+		} else {
+			this.speedCalc += 1;
+		}
+		
+	};
+
+Snake.prototype.gotFood = 
+	function() {
+		this.score += 1;
+		this.size += SNAKE_SIZE_INCREASE_FACTOR;
+		createNewFood();
+	};
+
+// End Class Snake
+//_____________________________________________________________________________________
 
 
 // On Load
 $(function() {
-
 	//console.log(arr.length >= SNAKE_START_LENGTH);
 	// Set Width and Height of Canvas
 	$('#canvas').css({'border-style': 'solid'})
@@ -109,186 +309,204 @@ $(function() {
 		draw();
 	}, 1000/FPS);
 	
-	$(document).bind("keydown", "left", function() {
-		if (direction != DIRECTION_LEFT && direction != DIRECTION_RIGHT) {
-			direction = DIRECTION_LEFT;
-		}
-	});
-
-	$(document).bind("keydown", "right", function() {
-		if (direction != DIRECTION_LEFT && direction != DIRECTION_RIGHT) {
-			direction = DIRECTION_RIGHT;
-		}
-	});
-
-	$(document).bind("keydown", "up", function() {
-		if (direction != DIRECTION_UP && direction != DIRECTION_DOWN) {
-			direction = DIRECTION_UP;
-		}
-	});
-
-	$(document).bind("keydown", "down", function() {
-		if (direction != DIRECTION_UP && direction != DIRECTION_DOWN) {
-			direction = DIRECTION_DOWN;
-		}
-	});
+	bindControls();
 });
-
 
 function initialize() {
 
-	// Set initial values
-	direction = DIRECTION_LEFT;
-	speedCalc = 0;
-	speed = SNAKE_START_SPEED;
-
-	foodPosition = null;	
 	score = 0;
 
-	snake.queue = [];
-	snake.size = SNAKE_START_LENGTH;
+	food = new Food();
 
-	board = [];
+	playerSnake1 = createSnakePlayer1();
+}
 
-	// Initialize Empty Board
-	for (var row = 0; row < BOARD_ROWS; row++) {
-		var rowArray = [];
-		for (var column = 0; column < BOARD_COLUMNS; column++) {
-			rowArray.push(EMPTY_BOX);
-		}
-
-		board.push(rowArray);
-	} 
-
-	// Draw Snake on Board
-	for (var columnShift = SNAKE_START_LENGTH - 1; columnShift >= 0; columnShift--) {
-		board[SNAKE_START_ROW][SNAKE_START_COLUMN + columnShift] = SNAKE_BOX;
-		snake.add([SNAKE_START_ROW, SNAKE_START_COLUMN + columnShift]);
-	}
-
-	// Create Random Food
-	createRandomFood();
+// This updates the game state
+function update() {
+	SnakesOnBoard.update();
 }
 
 
-// This function draws the board
+// This function draws the characters
 function draw() {
 
 	// Clear Rect
 	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-	// Draw Items
-	for (var row = 0; row < BOARD_ROWS; row++) {
-		var rowArray = board[row];
-		for (var column = 0; column < BOARD_COLUMNS; column++) {
+	// Draw Snake
+	SnakesOnBoard.draw(ctx);
 
-			var coord = getCoordsOfBox(row, column);
-			
-			if (rowArray[column] == EMPTY_BOX) {
-				//ctx.fillStyle = "#000";
-			} else if (rowArray[column] == SNAKE_BOX) {
-				ctx.fillStyle = "#0F0";
-				ctx.fillRect(coord.x, coord.y, BOX_WIDTH, BOX_HEIGHT);
-			} else if (rowArray[column] == FOOD_BOX) {
-				ctx.fillStyle = "#F00";
-				ctx.fillRect(coord.x, coord.y, BOX_WIDTH, BOX_HEIGHT);
-			}
-		}
-	}
+	// Draw Food
+	food.draw(ctx);
 
 	// Draw Score
 	ctx.fillStyle = "#00F";
 	ctx.font = 'italic 15pt Calibri';
-	ctx.fillText("Score : " + score, SCORE_LOCATION_X, SCORE_LOCATION_Y)
+	
+	if (player1Snake != null) {
+		ctx.fillText("Score : " + SnakesOnBoard.getSnake(player1Snake).score, SCORE_LOCATION_1_X, SCORE_LOCATION_1_Y);	
+	}
+	
+	if (player2Snake != null) {
+		ctx.fillText("Score : " + SnakesOnBoard.getSnake(player2Snake).score, SCORE_LOCATION_2_X, SCORE_LOCATION_2_Y);
+	}
 }
 
 // This function takes the coordinates of the board and draws a box
 function getCoordsOfBox(row, column) {
-	var coord = new Object();
+ 	var coord = new Object();
 	coord.x = column * BOX_WIDTH;
 	coord.y = row * BOX_HEIGHT;
 
 	return coord;
 }
 
+function getRandomColor() {
+	var index = Math.floor((Math.random() * RANDOM_COLORS.length - 1) + 0);
+	return RANDOM_COLORS[index];
+}
 
-// This function updates the position of the Snake
-function update() {
 
-	if (speedCalc >= speed) {
-		var snakeCurrentPositionX = snake.headX();
-		var snakeCurrentPositionY = snake.headY();
+// What happens when the snake eats the food
+function createNewFood() {
 
-		if (direction == DIRECTION_LEFT) {
-			snakeCurrentPositionX -= 1;		
-		} else if (direction == DIRECTION_RIGHT) {
-			snakeCurrentPositionX += 1;
-		} else if (direction == DIRECTION_UP) {
-			snakeCurrentPositionY -= 1;
-		} else {
-			snakeCurrentPositionY += 1;
-		}
+	// Create new Food
+	food = new Food();
+}
 
-		// Wrap Around
-		if (snakeCurrentPositionX < 0) {
-			snakeCurrentPositionX = BOARD_COLUMNS - 1;
-		} else if (snakeCurrentPositionX >= BOARD_COLUMNS) {
-			snakeCurrentPositionX = 0;
-		} else if (snakeCurrentPositionY < 0) {
-			snakeCurrentPositionY = BOARD_ROWS - 1;
-		} else if (snakeCurrentPositionY >= BOARD_ROWS) {
-			snakeCurrentPositionY = 0;
-		} 
+// Called when game is over 
+function gameOver(id) {
 
-		// Check to see if food found
-		if (snakeCurrentPositionX == foodPosition[1] && snakeCurrentPositionY == foodPosition[0]) {
-			gotFood();
-		}
+	SnakesOnBoard.remove(id);
 
-		// Check for collision
-		if (snake.contains([snakeCurrentPositionY, snakeCurrentPositionX])) {
-			alert("Game Over");
-			initialize();
-		}
+	if (id == player1Snake) {
+		alert("Game Over for Player 1");
+		player1Snake = null;
+	}
 
-		var tail = snake.add([snakeCurrentPositionY, snakeCurrentPositionX]);
+	if (id == player2Snake) {
+		alert("Game Over for Player 2");
+		player2Snake = null;
+	}
+}
+
+function bindControls() {
+	$(document).bind("keydown", "left", function() {
 		
-		if (tail != null) {
-			board[tail[0]][tail[1]] = EMPTY_BOX;				
+		playerSnake1 = createSnakePlayer1();
+
+		var currentSnake = SnakesOnBoard.getSnake(player1Snake);
+
+		if (currentSnake.direction != DIRECTION_LEFT && currentSnake.direction != DIRECTION_RIGHT) {
+			currentSnake.direction = DIRECTION_LEFT;
 		}
+	});
 
-		board[snakeCurrentPositionY][snakeCurrentPositionX] = SNAKE_BOX;
+	$(document).bind("keydown", "right", function() {		
+		
+		playerSnake1 = createSnakePlayer1();
 
-		speedCalc = 0;
+		var currentSnake = SnakesOnBoard.getSnake(player1Snake);
+
+		if (currentSnake.direction != DIRECTION_LEFT && currentSnake.direction != DIRECTION_RIGHT) {
+			currentSnake.direction = DIRECTION_RIGHT;
+		}
+	});
+
+	$(document).bind("keydown", "up", function() {
+		
+		playerSnake1 = createSnakePlayer1();
+
+		var currentSnake = SnakesOnBoard.getSnake(player1Snake);
+
+		if (currentSnake.direction != DIRECTION_UP && currentSnake.direction != DIRECTION_DOWN) {
+			currentSnake.direction = DIRECTION_UP;
+		}
+	});
+
+	$(document).bind("keydown", "down", function() {
+		
+		player1Snake = createSnakePlayer1();
+
+		var currentSnake = SnakesOnBoard.getSnake(player1Snake);
+
+		if (currentSnake.direction != DIRECTION_UP && currentSnake.direction != DIRECTION_DOWN) {
+			currentSnake.direction = DIRECTION_DOWN;
+		}
+	});
+
+	$(document).bind("keydown", "a", function() {
+		
+		playerSnake2 = createSnakePlayer2();
+
+		var currentSnake = SnakesOnBoard.getSnake(player2Snake);
+
+		if (currentSnake.direction != DIRECTION_LEFT && currentSnake.direction != DIRECTION_RIGHT) {
+			currentSnake.direction = DIRECTION_LEFT;
+		}
+	});
+
+	$(document).bind("keydown", "d", function() {		
+		
+		playerSnake2 = createSnakePlayer2();
+
+		var currentSnake = SnakesOnBoard.getSnake(playerSnake2);
+
+		if (currentSnake.direction != DIRECTION_LEFT && currentSnake.direction != DIRECTION_RIGHT) {
+			currentSnake.direction = DIRECTION_RIGHT;
+		}
+	});
+
+	$(document).bind("keydown", "w", function() {
+		
+		playerSnake2 = createSnakePlayer2();
+
+		var currentSnake = SnakesOnBoard.getSnake(playerSnake2);
+
+		if (currentSnake.direction != DIRECTION_UP && currentSnake.direction != DIRECTION_DOWN) {
+			currentSnake.direction = DIRECTION_UP;
+		}
+	});
+
+	$(document).bind("keydown", "s", function() {
+		
+		playerSnake2 = createSnakePlayer2();
+
+		var currentSnake = SnakesOnBoard.getSnake(playerSnake2);
+
+		if (currentSnake.direction != DIRECTION_UP && currentSnake.direction != DIRECTION_DOWN) {
+			currentSnake.direction = DIRECTION_DOWN;
+		}
+	});
+}
+
+
+function createSnakePlayer1() {
+	if (player1Snake == null) {
+		player1Snake = SnakesOnBoard.add();	
 	} else {
-		speedCalc += 1;
+		return player1Snake;
 	}
-	
 }
 
-// This function returns a random position for Food
-function createRandomFood() {
-	var row = Math.floor((Math.random() * BOARD_ROWS - 1) + 1);
-	var column = Math.floor((Math.random() * BOARD_COLUMNS - 1) + 1);
-
-	foodPosition = [row, column];
-	board[foodPosition[0]][foodPosition[1]] = FOOD_BOX;
-}
-
-function gotFood() {
-	score += 1;
-	snake.size += SNAKE_SIZE_INCREASE_FACTOR;
-
-	increaseSpeed();
-	// Create Random Food
-	createRandomFood();
-}
-
-function increaseSpeed() {
-
-	if (score > 5 && speed > 0) {
-		speed = speed - (score / 5);
-		if (speed < 0)
-			speed = 0;
+function createSnakePlayer2() {
+	if (player2Snake == null) {
+		player2Snake = SnakesOnBoard.add();	
+	} else {
+		return player2Snake;
 	}
+}
+
+function collisionCheck(snake, snakeCurrentPositionX, snakeCurrentPositionY) {
+
+	// Check to see if food found
+	if (snakeCurrentPositionX == food.location[1] && snakeCurrentPositionY == food.location[0]) {
+		snake.gotFood();
+	}
+
+	// Check for collision with snakes
+	if (SnakesOnBoard.contains([snakeCurrentPositionY, snakeCurrentPositionX])) {
+		gameOver(snake.id);
+	}
+
 }
